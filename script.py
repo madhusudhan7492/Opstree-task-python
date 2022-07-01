2ad1eb39b49a8c6c993b5f4f90fac844b6d0f094
MadhuSudhan <chmadhus97@gmail.com>
2022-07-01 20:15:41 +0530

    updated script

--- script.py
+++ script.py
@@ -22,10 +22,12 @@ describe_instance =client.describe_instances()
 
 Instance_Id = []
 Instance_Type = ""
+describe_instance_i = ""
 for i in describe_instance['Reservations']:
     for j in i['Instances']:
         for tag in j['Tags']:
             if tag['Value'] == "Testing_Server":
+                describe_instance_i = client.describe_instances(InstanceIds=[j['InstanceId']])
                 print('{} has tag {}'.format(j['InstanceId'], tag['Value']))
                 print('Instance type of {} is {}'.format(j['InstanceId'], j['InstanceType']))
                 Instance_Id.append(j['InstanceId'])
@@ -35,17 +37,17 @@ for i in describe_instance['Reservations']:
 
 # In[66]:
 
+current_state = describe_instance_i['Reservations'][0]['Instances'][0]['State']['Name']
 
 import subprocess
 import time
 for instance in Instance_Id:
-    describe_instance_i =client.describe_instances(InstanceIds=[instance])
-    current_state = describe_instance_i['Reservations'][0]['Instances'][0]['State']['Name']
+    
     response = client.stop_instances(InstanceIds=[instance])
     for state in response['StoppingInstances']:
         current_state = state['CurrentState']['Name']
         print("After stopping current state is:: ",current_state)
-        while current_state == "stopped":
+        while current_state == "stopping":
             time.sleep(25)
             modify_instance = client.modify_instance_attribute(InstanceId=instance,InstanceType={'Value':'t2.micro'})
             if (modify_instance['ResponseMetadata']['HTTPStatusCode'] == 200):
