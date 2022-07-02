2c5e008dda3a401f904622c2e35133ba113962ac
MadhuSudhan <chmadhus97@gmail.com>
2022-07-03 01:10:10 +0530

    updated file names

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,32 +11,32 @@ pipeline {
       }
     }
 
-    stage('Run python script') {
+    stage('Run Start and Change Instance Type script') {
       steps {
 
         withCredentials([
           [$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-creds', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']
         ]) {
-          sh '/usr/bin/python3 -u script.py'
+          sh '/usr/bin/python3 -u startAndChangeInstance.py'
         }
 
       }
     }
 
-    stage('run ansible playbook') {
+    stage('Run ansible playbook') {
       steps {
         //here main.yml file is in the cloned repository
         ansiblePlaybook credentialsId: 'ansadmin', disableHostKeyChecking: true, installation: 'ansible', playbook: 'main.yml'
       }
     }
 
-     stage('Run flipback script') {
+     stage('Run flipback and start instance script') {
       steps {
 
         withCredentials([
           [$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws-creds', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']
         ]) {
-          sh '/usr/bin/python3 -u flipback.py'
+          sh '/usr/bin/python3 -u flipbackAndStartInstance.py'
         }
 
       }
--- startAndChangeInstance.py
+++ startAndChangeInstance.py
@@ -31,16 +31,16 @@ for i in describe_instance['Reservations']:
 
 current_state = describe_instance_i['Reservations'][0]['Instances'][0]['State']['Name']
 
-
 t = Texttable()
 for instance in Instance_Id:
     
     response = client.stop_instances(InstanceIds=[instance])
     for state in response['StoppingInstances']:
+        describe_instance_instance = client.describe_instances(InstanceIds=[instance])
         current_state_0 = state['CurrentState']['Name']
         print("*"*60)
         print("<<<<After Stopping the instance>>>>")
-        t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_0]])
+        t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_0]])
         print(t.draw())
         while current_state_0 == "stopping" or current_state_0 == "stopped":
             time.sleep(35)
@@ -50,14 +50,14 @@ for instance in Instance_Id:
                 current_state_1 = describe_current_state_instance['Reservations'][0]['Instances'][0]['State']['Name']
                 print("*"*60)
                 print("<<<<After changing the instance type>>>>")
-                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_1]])
+                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_1]])
                 print(t.draw())
                 print("*"*60)
                 print("<<<<Starting the instance>>>>")
                 start_response = client.start_instances(InstanceIds = [instance])
                 time.sleep(35)
                 current_state_2 = start_response['StartingInstances'][0]['CurrentState']['Name']
-                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_2]])
+                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_2]])
                 print(t.draw())
                 print("*"*60)
 
