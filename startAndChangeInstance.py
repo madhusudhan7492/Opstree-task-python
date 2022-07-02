805b918e425445552650d6c6ddf96777d22d9870
MadhuSudhan <chmadhus97@gmail.com>
2022-07-03 01:28:00 +0530

    updated scripts

--- startAndChangeInstance.py
+++ startAndChangeInstance.py
@@ -36,36 +36,28 @@ for instance in Instance_Id:
     
     response = client.stop_instances(InstanceIds=[instance])
     for state in response['StoppingInstances']:
-        describe_instance_instance = client.describe_instances(InstanceIds=[instance])
+        describe_instance_ = client.describe_instances(InstanceIds=[instance])
         current_state_0 = state['CurrentState']['Name']
         print("*"*60)
         print("<<<<After Stopping the instance>>>>")
-        t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_0]])
+        t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_['Reservations'][0]['Instances'][0]['InstanceType'],current_state_0]])
         print(t.draw())
         while current_state_0 == "stopping" or current_state_0 == "stopped":
             time.sleep(35)
             modify_instance = client.modify_instance_attribute(InstanceId=instance,InstanceType={'Value':InstanceType})
             if (modify_instance['ResponseMetadata']['HTTPStatusCode'] == 200):
+                describe_instance_1 = client.describe_instances(InstanceIds=[instance])
                 describe_current_state_instance = client.describe_instances(InstanceIds=[instance])
                 current_state_1 = describe_current_state_instance['Reservations'][0]['Instances'][0]['State']['Name']
                 print("*"*60)
                 print("<<<<After changing the instance type>>>>")
-                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_1]])
+                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_1['Reservations'][0]['Instances'][0]['InstanceType'],current_state_1]])
                 print(t.draw())
                 print("*"*60)
                 print("<<<<Starting the instance>>>>")
                 start_response = client.start_instances(InstanceIds = [instance])
                 time.sleep(35)
                 current_state_2 = start_response['StartingInstances'][0]['CurrentState']['Name']
-                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_instance['Reservations'][0]['Instances'][0]['InstanceType'],current_state_2]])
+                t.add_rows([['Name','Instance_Id', 'Instance_Type','Instance_State'], [Instance_Name,instance ,describe_instance_1['Reservations'][0]['Instances'][0]['InstanceType'],current_state_2]])
                 print(t.draw())
                 print("*"*60)
-
-    
-
-            
-            
-
-    
-    
-
